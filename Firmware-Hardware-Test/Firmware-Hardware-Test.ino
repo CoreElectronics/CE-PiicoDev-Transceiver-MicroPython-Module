a90270d4007f5556e9a58096b90480ddff413687
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-09-08 11:55:39 +1000

    Working perfectly

--- Firmware-Hardware-Test/Firmware-Hardware-Test.ino
+++ Firmware-Hardware-Test/Firmware-Hardware-Test.ino
@@ -1,39 +1,10 @@
-// RFM69HCW Example Sketch
-// Send serial input characters from one RFM69 node to another
-// Based on RFM69 library sample code by Felix Rusu
-// http://LowPowerLab.com/contact
-// Modified for RFM69HCW by Mike Grusin, 4/16
-
-// This sketch will show you the basics of using an
-// RFM69HCW radio module. SparkFun's part numbers are:
-// 915MHz: https://www.sparkfun.com/products/12775
-// 434MHz: https://www.sparkfun.com/products/12823
-
-// See the hook-up guide for wiring instructions:
-// https://learn.sparkfun.com/tutorials/rfm69hcw-hookup-guide
-
-// Uses the RFM69 library by Felix Rusu, LowPowerLab.com
-// Original library: https://www.github.com/lowpowerlab/rfm69
-// SparkFun repository: https://github.com/sparkfun/RFM69HCW_Breakout
-
-// Include the RFM69 and SPI libraries:
-
 #include <RFM69.h>
 #include <SPI.h>
-
-// Addresses for this node. CHANGE THESE FOR EACH NODE!
 
 #define NETWORKID     0   // Must be the same for all nodes
 #define MYNODEID      1   // My node ID
 #define TONODEID      2   // Destination node ID
-
-// RFM69 frequency, uncomment the frequency of your module:
-
-//#define FREQUENCY   RF69_433MHZ
 #define FREQUENCY     RF69_915MHZ
-
-// AES encryption (or not):
-
 #define ENCRYPT       true // Set to "true" to use encryption
 #define ENCRYPTKEY    "TOPSECRETPASSWRD" // Use the same 16-byte key on all nodes
 
