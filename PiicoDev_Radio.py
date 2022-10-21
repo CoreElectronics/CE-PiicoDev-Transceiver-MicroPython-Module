c141c2ae40c2f94d5c7797b29f75d58c2803ebb7
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-10-21 13:47:53 +1100

    1st Success
    Data sent from Python 1 and successfully received at Python 2

--- PiicoDev_Radio.py
+++ PiicoDev_Radio.py
@@ -85,23 +85,20 @@ class PiicoDev_Radio(object):
         self._write(register, int.to_bytes(integer, length, 'big'))
         
     def send_bytes(self, message):
-        print('message written, now time to send the length')
-        print('message length ' + str(len(message)))
+        #print('message length ' + str(len(message)))
         self._write_int(_set_bit(_REG_PAYLOAD_LENGTH, 7), len(message))
         sleep_ms(1000)
         self._write(_set_bit(_REG_PAYLOAD, 7), message)
         sleep_ms(1000)
-        print('---')
+        #print('---')
         self._write_int(_set_bit(_REG_PAYLOAD_GO, 7), 1)
         
     def receive_bytes(self):
-        print('about to run payload length')
+        #print('about to run payload length')
         payload_length = self._read_int(_REG_PAYLOAD_LENGTH)
-        print('payload length' + str(payload_length))
-        print('finished running payload length')
+        print('RECEIVED_PAYLOAD_LENGTH:' + str(payload_length))
         message = self._read(_REG_PAYLOAD, length=payload_length)
-        print('finished receiving payload')
-        print('PAYLOAD:' + str(message))
+        print('RECEIVED PAYLOAD:' + str(message))
         return message
     
     def on(self):
@@ -171,8 +168,8 @@ class PiicoDev_Radio(object):
     
     @property
     def message(self):
-        #message_string = str(self.receive_bytes(), 'utf8')
-        message_string = str(self.receive_bytes())
+        message_string = str(self.receive_bytes(), 'utf8')
+        #message_string = str(self.receive_bytes())
         return message_string
     
     @message.setter
