ec2e47986461a34f20a8110607d488cf93e028d1
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-10-28 10:09:09 +1100

    no missing packets

--- Firmware-Hardware-Test/Firmware-Hardware-Test.ino
+++ Firmware-Hardware-Test/Firmware-Hardware-Test.ino
@@ -1,16 +1,17 @@
 #include <RFM69.h>
 #include <SPI.h>
 
-#define NETWORKID     0   // Must be the same for all nodes
-#define MYNODEID      1   // My node ID
-#define TONODEID      2   // Destination node ID
-#define FREQUENCY     RF69_915MHZ
-#define ENCRYPTKEY    "-PiicoDevRadio- " // Use the same 16-byte key on all nodes
+#define NETWORKID 0  // Must be the same for all nodes
+#define MYNODEID 1   // My node ID
+#define TONODEID 0   // Destination node ID
+#define FREQUENCY RF69_915MHZ
+#define ENCRYPTKEY "-PiicoDevRadio- "  // Use the same 16-byte key on all nodes
 
 RFM69 radio;
 
-void setup()
-{
+
+
+void setup() {
   // Open a serial port so we can send keystrokes to the module:
   Serial.begin(115200);
   Serial.print("Node ");
@@ -22,13 +23,21 @@ void setup()
   radio.encrypt(ENCRYPTKEY);
 }
 
-void loop()
-{
+int sendNow = 0;
+long millisPrev = 0;
+uint8_t counter = 0;
+
+void loop() {
   // Set up a "buffer" for characters that we'll send:
 
   static char sendbuffer[62];
   static int sendlength = 0;
 
+
+  if ((millis() - millisPrev) > 1000) {
+    sendNow = 1;
+    millisPrev = millis();
+  }
   // SENDING
 
   // In this section, we'll gather serial characters and
@@ -37,39 +46,30 @@ void loop()
 
   // If there is any serial input, add it to the buffer:
 
-  if (Serial.available() > 0)
-  {
-    char input = Serial.read();
 
-    if (input != '\r') // not a carriage return
-    {
-      sendbuffer[sendlength] = input;
-      sendlength++;
+
+  if (sendNow == 1) {
+    // Send the packet!
+
+    Serial.print("sending to node ");
+    Serial.print(TONODEID, DEC);
+    Serial.print(", message [");
+    Serial.print(counter);
+    Serial.println("]");
+
+    // There are two ways to send packets. If you want
+    // acknowledgements, use sendWithRetry():
+
+    // if (radio.sendWithRetry(TONODEID, counter, sendlength))
+    //   Serial.println("ACK received!");
+    // else
+    //   Serial.println("no ACK received");
+    radio.send(TONODEID, "1", 1);
+    counter++;
+    if (counter == 255) {
+      counter = 0;
     }
-
-    // If the input is a carriage return, or the buffer is full:
-
-    if ((input == '\r') || (sendlength == 61)) // CR or buffer full
-    {
-      // Send the packet!
-
-      Serial.print("sending to node ");
-      Serial.print(TONODEID, DEC);
-      Serial.print(", message [");
-      for (byte i = 0; i < sendlength; i++)
-        Serial.print(sendbuffer[i]);
-      Serial.println("]");
-
-      // There are two ways to send packets. If you want
-      // acknowledgements, use sendWithRetry():
-
-      if (radio.sendWithRetry(TONODEID, sendbuffer, sendlength))
-        Serial.println("ACK received!");
-      else
-        Serial.println("no ACK received");
-
-      sendlength = 0; // reset the packet
-    }
+    sendNow = 0;
   }
 
   // RECEIVING
@@ -77,7 +77,7 @@ void loop()
   // In this section, we'll check with the RFM69HCW to see
   // if it has received any packets:
 
-  if (radio.receiveDone()) // Got one!
+  if (radio.receiveDone())  // Got one!
   {
     // Print out the information:
 
@@ -97,8 +97,7 @@ void loop()
     Serial.print("], RSSI ");
     Serial.println(radio.RSSI);
 
-    if (radio.ACKRequested())
-    {
+    if (radio.ACKRequested()) {
       radio.sendACK();
       Serial.println("ACK sent");
     }
