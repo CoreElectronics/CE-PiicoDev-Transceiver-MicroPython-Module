4286fa519eb287572c9aed8f0f18238533705020
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-11-23 14:50:19 +1100

    Update soak_test_repeater.py

--- soak_test_repeater.py
+++ soak_test_repeater.py
@@ -3,15 +3,8 @@ from PiicoDev_Unified import sleep_ms
 
 radio = PiicoDev_Radio()
 
-i = 0
-key = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOP'
-# key = 'abcd'
-# key = 'abcdefghijklmno'
 while True:
-#    if radio.receive():
-    i = i + 1
-#         print(str(radio.key) + ":" + str(radio.rssi))
-    # sleep_ms(20)
-#         radio.send(radio.key, radio.value)
-    radio.send(key, i)
-    sleep_ms(10000)
+    if radio.receive():
+        print(str(radio.value) + ': ' + str(radio.key) + " RSSI:" + str(radio.rssi))
+        radio.send(radio.key, radio.value)
+    sleep_ms(5)
