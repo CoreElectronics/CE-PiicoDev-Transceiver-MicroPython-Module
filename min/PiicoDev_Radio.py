1aa6e25395e690ec16c7ecb241cd66bdfcab5b8f
GitHub Action <action@github.com>
2022-11-30 06:01:55 +0000

    Update min/PiicoDev_Radio.py

--- min/PiicoDev_Radio.py
+++ min/PiicoDev_Radio.py
@@ -33,7 +33,7 @@ def _set_bit(x,n):return x|1<<n
 def debug(text):
 	if DEBUG:print(text)
 class PiicoDev_Radio:
-	def __init__(self,bus=_A,freq=_A,sda=_A,scl=_A,address=_BASE_ADDRESS,id=_A,radio_address=radio_config.radio_address,channel=radio_config.channel,suppress_warnings=False):
+	def __init__(self,bus=_A,freq=_A,sda=_A,scl=_A,address=_BASE_ADDRESS,id=_A,radio_address=1,channel=0,suppress_warnings=False):
 		try:
 			if compat_ind>=1:0
 			else:print(compat_str)
@@ -41,7 +41,7 @@ class PiicoDev_Radio:
 		self.i2c=create_unified_i2c(bus=bus,freq=freq,sda=sda,scl=scl,suppress_warnings=suppress_warnings);self._address=address
 		if type(id)is list and not all((v==0 for v in id)):assert max(id)<=1 and min(id)>=0 and len(id)==4,'id must be a list of 1/0, length=4';self._address=8+id[0]+2*id[1]+4*id[2]+8*id[3]
 		else:self._address=address
-		self.radio_address=radio_address;self.channel=channel;self.destination_radio_address=radio_config.destination_radio_address;self.rssi=0;self.type=0;self.message='';self.key='';self.value=_A;self.radio_source_address=0;self.destination_radio_address=0;self.rx_channel=0;self.rx_destination_radio_address=0
+		self._write_int(_REG_RFM69_NODE_ID,radio_address);self._write_int(_REG_RFM69_NETWORK_ID,channel);self.rssi=0;self.type=0;self.message='';self.key='';self.value=_A;self.source_radio_address=0
 		try:
 			if self.whoami!=_DEVICE_ID:print('* Incorrect device found at address {}'.format(address))
 		except:print("* Couldn't find a device - check switches and wiring")
@@ -57,13 +57,13 @@ class PiicoDev_Radio:
 		else:return int.from_bytes(data,_B)
 	def _write_int(self,register,integer,length=1):self._write(register,int.to_bytes(integer,length,_B))
 	def send_payload(self,payload):
-		payload_list=[payload[i:i+_MAXIMUM_I2C_SIZE-1]for i in range(0,len(payload),_MAXIMUM_I2C_SIZE-1)];self._write_int(_REG_PAYLOAD_LENGTH,len(payload));sleep_ms(5)
-		for i in range(len(payload_list)):self._write(_REG_PAYLOAD,payload_list[i]);sleep_ms(5)
+		payload_list=[payload[i:i+_MAXIMUM_I2C_SIZE-1]for i in range(0,len(payload),_MAXIMUM_I2C_SIZE-1)];self._write_int(_REG_PAYLOAD_LENGTH,len(payload));print('Payload Length:'+str(len(payload)));sleep_ms(5)
+		for i in range(len(payload_list)):self._write(_REG_PAYLOAD,payload_list[i]);sleep_ms(5);print('payload'+str(payload_list[i]))
 		self._write_int(_REG_PAYLOAD_GO,1)
 	def receive_payload(self):
 		payload_length=0;payload=bytes(0)
 		if self._payload_new==1:
-			payload_length=self._read_int(_REG_PAYLOAD_LENGTH)+1;unprocessed_payload_length=payload_length;sleep_ms(5);number_of_chunks=int(truncate(payload_length/_MAXIMUM_I2C_SIZE))+1
+			sleep_ms(100);payload_length=self._read_int(_REG_PAYLOAD_LENGTH)+2;unprocessed_payload_length=payload_length;sleep_ms(5);number_of_chunks=int(truncate(payload_length/_MAXIMUM_I2C_SIZE))+1
 			for i in range(number_of_chunks):
 				chunk_length=_MAXIMUM_I2C_SIZE
 				if unprocessed_payload_length<32:chunk_length=unprocessed_payload_length
@@ -80,17 +80,13 @@ class PiicoDev_Radio:
 		if value>20:value=20
 		self._write(_REG_TX_POWER,pack('b',value))
 	@property
-	def rfm69_network_id(self):return self._read_int(_REG_RFM69_NETWORK_ID)
-	@rfm69_network_id.setter
-	def rfm69_network_id(self,value):print('channel setter called');self._write_int(_REG_RFM69_NETWORK_ID,value)
+	def channel(self):' There is no setter because we only want to set when initialising because changing this will trigger a re-initialise in the arduino';return self._read_int(_REG_RFM69_NETWORK_ID)
 	@property
-	def rfm69_node_id(self):return self._read_int(_REG_RFM69_NODE_ID);print('Node ID Called')
-	@rfm69_node_id.setter
-	def rfm69_node_id(self,value):self._write_int(_REG_RFM69_NODE_ID,value);print('Set Node ID Called')
+	def radio_address(self):' There is no setter because we only want to set when initialising because changing this will trigger a re-initialise in the arduino';return self._read_int(_REG_RFM69_NODE_ID);print('Node ID Called')
 	@property
-	def rfm69_to_node_id(self):return self._read_int(_REG_RFM69_TO_NODE_ID)
-	@rfm69_to_node_id.setter
-	def rfm69_to_node_id(self,value):debug('Setting destination radio address to '+str(value)+'.');self._write_int(_REG_RFM69_TO_NODE_ID,value)
+	def destination_radio_address(self):return self._read_int(_REG_RFM69_TO_NODE_ID)
+	@destination_radio_address.setter
+	def destination_radio_address(self,value):debug('Setting destination radio address to '+str(value)+'.');self._write_int(_REG_RFM69_TO_NODE_ID,value)
 	def rfm69_reset(self):debug('Resetting RFM69');self._write_int(_REG_RFM69_RESET,1)
 	@property
 	def payload_length(self):return 0
@@ -101,21 +97,19 @@ class PiicoDev_Radio:
 	def receive(self):
 		payload_length,payload=self.receive_payload()
 		if payload_length!=0:
-			payload_bytes=bytes(payload);self.rx_channel=int.from_bytes(payload_bytes[1:2],_B);self.rx_destination_radio_address=int.from_bytes(payload_bytes[3:4],_B)
-			if self.rx_channel==self.channel and(self.rx_destination_radio_address==0 or self.rx_destination_radio_address==self.radio_address):
-				self.rssi=-int.from_bytes(payload_bytes[:1],_B);self.source_radio_address=int.from_bytes(payload_bytes[2:3],_B);self.type=int.from_bytes(payload_bytes[4:5],_B)
-				try:
-					if self.type==1:self.key=str(payload_bytes[10:],_C);self.value=unpack('i',payload_bytes[5:9])[0]
-					if self.type==2:self.key=str(payload_bytes[10:],_C);self.value=unpack('f',payload_bytes[5:9])[0]
-					if self.type==3:self.message=str(payload_bytes[6:],_C)
-				except:print('* error parsing payload')
-				return True
+			payload_bytes=bytes(payload);print(payload_bytes);self.rssi=-int.from_bytes(payload_bytes[:1],_B);self.source_radio_address=int.from_bytes(payload_bytes[1:2],_B);self.type=int.from_bytes(payload_bytes[2:3],_B)
+			try:
+				if self.type==1:self.key=str(payload_bytes[8:],_C);self.value=unpack('>i',payload_bytes[3:7])[0]
+				if self.type==2:self.key=str(payload_bytes[8:],_C);self.value=unpack('>f',payload_bytes[3:7])[0]
+				if self.type==3:self.message=str(payload_bytes[3:],_C)
+			except:print('* error parsing payload')
+			return True
 		return False
 	def send(self,message_string,value=_A,address=0):
-		A='s';destination_radio_address=address
-		if isinstance(value,int):type=1;message_string=message_string[:_MAXIMUM_PAYLOAD_LENGTH-9];format_characters='BBBBiB'+str(len(message_string))+A;data=pack(format_characters,self.channel,self.radio_address,destination_radio_address,type,value,len(message_string),bytes(message_string,_C))
-		if isinstance(value,float):type=2;message_string=message_string[:_MAXIMUM_PAYLOAD_LENGTH-9];format_characters='BBBBfB'+str(len(message_string))+A;data=pack(format_characters,self.channel,self.radio_address,destination_radio_address,type,value,len(message_string),bytes(message_string,_C))
-		if value is _A:type=3;message_string=message_string[:_MAXIMUM_PAYLOAD_LENGTH-5];format_characters='BBBBB'+str(len(message_string))+A;data=pack(format_characters,self.channel,self.radio_address,destination_radio_address,type,len(message_string),bytes(message_string,_C))
+		A='s';debug('sending stuff');self.destination_radio_address=address
+		if isinstance(value,int):type=1;message_string=message_string[:_MAXIMUM_PAYLOAD_LENGTH-6];format_characters='>BiB'+str(len(message_string))+A;data=pack(format_characters,type,value,len(message_string),bytes(message_string,_C))
+		if isinstance(value,float):type=2;message_string=message_string[:_MAXIMUM_PAYLOAD_LENGTH-6];format_characters='>BfB'+str(len(message_string))+A;data=pack(format_characters,type,value,len(message_string),bytes(message_string,_C))
+		if value is _A:type=3;message_string=message_string[:_MAXIMUM_PAYLOAD_LENGTH-2];format_characters='>BB'+str(len(message_string))+A;data=pack(format_characters,type,len(message_string),bytes(message_string,_C))
 		self.send_payload(data)
 	def send_byte(self,value):data=pack('B',value);self.send_payload(data)
 	def receive_byte(self):
