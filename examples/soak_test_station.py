ab0337d98ff3eb2c4599af0f3d8abc79f4ee8ae5
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-12-19 12:55:03 +1100

    Update soak_test_station.py

--- examples/soak_test_station.py
+++ examples/soak_test_station.py
@@ -1,10 +1,10 @@
-from PiicoDev_Radio import PiicoDev_Radio
+from PiicoDev_Transceiver import PiicoDev_Transceiver
 from PiicoDev_Unified import sleep_ms
 from time import ticks_ms
 
 debugging=False
 
-radio = PiicoDev_Radio(radio_address=127, channel=0, debug=debugging)
+radio = PiicoDev_Transceiver(radio_address=0, channel=0)
 
 i = 0
 key = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRS'
@@ -18,30 +18,10 @@ print('Firmware:       ' + str(firmware[0]) + '.' + str(firmware[1]))
 print('Tx Power:       ' + str(int(radio.tx_power)))
 print('Radio Address   ' + str(radio.radio_address))
 print('Channel:        ' + str(radio.channel))
-sleep_ms(3000)
-radio_state = True
-radio.on()
-radio.tx_power = 21
-print('radio.tx_power:' + str(radio.tx_power))
-#radio.get_frequency()
-#sleep_ms(1000)
-radio.radio_frequency = 915
-print(radio.radio_frequency)
-radio.radio_frequency = 918
-print(radio.radio_frequency)
-radio.radio_frequency = 922
-print(radio.radio_frequency)
-radio.radio_frequency = 925
-print(radio.radio_frequency)
-radio.radio_frequency = 928
-print(radio.radio_frequency)
-radio.radio_frequency = 922
-radio.speed = 2
 
 while i < 10000:
     i += 1
-    radio.send(key,i,address=0)
-    #sleep_ms(1000)
+    radio.send(key,i,address=2)
     sleep_ms(100)  #200
     if radio.receive():
         if (radio.value != value_prev + 1 or not radio.key == key) and i > 2:
