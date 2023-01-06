94a73df558c4475808cfc16b2332fd99e6c5687c
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2023-01-06 12:58:29 +1100

    update example

--- examples/multiple_transceivers.py
+++ examples/multiple_transceivers.py
@@ -1,24 +1,22 @@
-# Demonstrate how to use the send() command
-# send() will accept strings, values, and "named values" which
-# are string,value pairs.
+# Demonstrate how to use multiple PiicoDev Transceivers on the same bus
 
 from PiicoDev_Transceiver import PiicoDev_Transceiver
 from PiicoDev_Unified import sleep_ms
 from math import sin
 
 # Initialise two separate radios
-transmitter = PiicoDev_Transceiver(id=[0,0,0,0])
+transmitter = PiicoDev_Transceiver(id=[0,0,0,0]) # each radio needs a unique ID switch setting
 receiver    = PiicoDev_Transceiver(id=[1,0,0,0])
 
-x = 0
+x = 0 # generates a sine wave
 
 while True:
     # TRANSMIT a new value from one Transceiver
     y = sin(x)                           # generate a new number
     transmitter.send( ('sine wave', y) ) # send the number
-    x += 0.2                             # increment the control variable
+    x += 0.1                             # increment the control variable
         
-    sleep_ms(100)
+    sleep_ms(50)
     
     # RECEIVE any incoming messages and print
     if receiver.receive():
